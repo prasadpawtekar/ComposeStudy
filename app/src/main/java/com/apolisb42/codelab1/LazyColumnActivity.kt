93fa7dee412c06246accab23a881c2f2a3cb06a6
Prasad Pawtekar <prasadpawtekra@gmail.com>
2023-10-10 01:39:19 +0530

    Codelab1 - Completed

--- app/src/main/java/com/apolisb42/codelab1/LazyColumnActivity.kt
+++ app/src/main/java/com/apolisb42/codelab1/LazyColumnActivity.kt
@@ -1,8 +1,13 @@
 package com.apolisb42.codelab1
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +15,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apolisb42.codelab1.ui.theme.CodeLab1Theme
+import kotlin.math.exp
+import kotlin.random.Random
 
 class LazyColumnActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +48,7 @@ class LazyColumnActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val list = List(100){"Player $it"}
+                    val list = List(100) { "$it" }
                     Players(list = list)
                 }
             }
@@ -42,7 +57,7 @@ class LazyColumnActivity : ComponentActivity() {
 }
 
 @Composable
-fun Players(modifier: Modifier= Modifier, list: List<String>) {
+fun Players(modifier: Modifier = Modifier, list: List<String>) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = list) {
             GreetingCard(name = it)
@@ -50,7 +65,10 @@ fun Players(modifier: Modifier= Modifier, list: List<String>) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
 fun GreetingPreview2() {
     CodeLab1Theme {
@@ -65,32 +83,69 @@ fun GreetingCard(name: String, modifier: Modifier = Modifier) {
         mutableStateOf(false)
     }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxWidth()
     ) {
-
-        Row(modifier = Modifier.padding(16.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
 
                 Text(
-                    text = "Hello $name!",
+                    text = "Player",
                     modifier = modifier
                 )
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+                if (expanded.value) {
+                    Text(
+                        text = "Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ".repeat(4)
+                    )
+                }
             }
-            ElevatedButton(
+            /*ElevatedButton(
                 onClick = {
                     expanded.value = !expanded.value
                 },
             ) {
                 Text(text = if (expanded.value) "Show Less" else "Show More")
+            }*/
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+
+                    contentDescription = if (expanded.value)
+                        stringResource(id = R.string.show_less)
+                    else stringResource(id = R.string.show_more)
+                )
             }
         }
     }
